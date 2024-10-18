3066cace09e6176d0b33a2d6a7815f9fd9b05b96
Sebas <103141905+sebas716@users.noreply.github.com>
2024-10-18 10:30:24 -0500

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Hello, Jenkins Pipeline!</h1>
+        <p>This is a basic React app to test the Jenkins pipeline.</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
