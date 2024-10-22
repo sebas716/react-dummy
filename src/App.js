acf293a000b37d35df250ecc40836c56ddd4c57b
Sebas <103141905+sebas716@users.noreply.github.com>
2024-10-22 17:10:49 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Hello, Jenkins Pipeline! v2</h1>
+        <h1>Hello, Jenkins Pipeline! v3</h1>
         <p>This is a basic React app to test the Jenkins pipeline.</p>
       </header>
     </div>
